6b870fa3a723ecdf93c1383297e24d806c53c89b
Ju5tG <134755669+Ju5tG@users.noreply.github.com>
2023-11-29 21:00:37 -0600

    Added comments

--- PipeGetADGroupMembertoGetADUser.ps1
+++ PipeGetADGroupMembertoGetADUser.ps1
@@ -1,1 +1,4 @@
-Get-ADGroupMember -Identity "Administrators" | Get-ADUser -Properties * | Select-Object Description, Enabled, DistinguishedName, SamAccountName, @{Name='LastLogon';Expression={[DateTime]::FromFileTime($_.LastLogon)}}, Manager
+#AD Group you want to iterate through
+Get-ADGroupMember -Identity "Domain Users" | Get-ADUser -Properties * | Select-Object Description, Enabled, DistinguishedName, SamAccountName, @{Name='LastLogon';Expression={[DateTime]::FromFileTime($_.LastLogon)}}, Manager | Format-Table
+#Pipes output to Get-ADUser with all properties available
+#Select which objects you want to output, @{Name='LastLogon';Expression={[DateTime]::FromFileTime($_.LastLogon)}} is a calculated value for LastLogonDate 
