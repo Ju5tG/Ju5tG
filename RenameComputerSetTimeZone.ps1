e0273e47ec0fb6217370b23f9676f23fe91d3285
Ju5tG <134755669+Ju5tG@users.noreply.github.com>
2023-12-16 13:27:25 -0600

    Update RenameComputerSetTimeZone.ps1
    Set timezone before reboot

--- RenameComputerSetTimeZone.ps1
+++ RenameComputerSetTimeZone.ps1
@@ -1,8 +1,8 @@
+tzutil /s "Central Standard time"
 $vm = Get-WmiObject -Class Win32_ComputerSystem
 if ($vm.Model -match "Virtual") {
     $vmName = Read-Host "Enter the name of the Hyper-V VM"
     Rename-Computer -NewName $vmName -Restart
-    tzutil /s "Central Standard time"
-} else {
+    } else {
     Write-Output "This machine is hosted on physical hardware."
 }
