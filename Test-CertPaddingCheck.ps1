a24ea44a09e44895d5ea6d015796f07b021970e8
Ju5tG <134755669+Ju5tG@users.noreply.github.com>
2023-12-18 19:18:50 -0600

    Changed to Get-ItemProperty

--- Test-CertPaddingCheck.ps1
+++ Test-CertPaddingCheck.ps1
@@ -1,2 +1,2 @@
-Test-RegistryKeyValue -Path 'HKLM:\Software\Microsoft\Cryptography\Wintrust\Config' -Name 'EnableCertPaddingCheck'
-Test-RegistryKeyValue -Path 'HKLM:\Software\WOWNOD6432\Microsoft\Cryptography\Wintrust\Config' -Name 'EnableCertPaddingCheck'
+Get-ItemProperty -Path 'HKLM:\Software\Microsoft\Cryptography\Wintrust\Config' -Name 'EnableCertPaddingCheck'
+Get-ItemProperty -Path 'HKLM:\Software\WOWNOD6432\Microsoft\Cryptography\Wintrust\Config' -Name 'EnableCertPaddingCheck'
